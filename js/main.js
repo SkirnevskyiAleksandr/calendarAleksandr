f0935e1b1924fde300f40f3bd785240a1c4fb1e6
chel13 <sanya-sha2016@ukr.net>
2020-10-18 04:47:44 +0300

    4

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ $('.owl-carousel').owlCarousel({
     items:1,
     loop:true,
     dots:false,
+    lazyLoad : true,
     autoplayHoverPause: false,
     smartSpeed: 1000,
-    nav : true,
+    nav:true,
   pagination : true
   })
   
